868ae136d181723eec1b7ffc893cd6f9f307dac8
unknown <abece85@gmail.com>
2020-04-28 20:30:42 -0700

    adds a Jupyter notebook which will be used for Capstone Project

---
+++ Capstone_Project_Using_Jupyter_Notebook.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This notebook will be used for Capstone Project
+
+# In first step a Jupyter notebook with simple Python code is pushed to GitHub.
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+import numpy as np
+
+
+# In[ ]:
+
+
+print ("Hello Capstone Project Course!")
+
